00b7b3b3cbe34dbc6a9bb61a8dbba7f6aa7ab3fc
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-17 12:24:04 +0000

    deploy: 2a2753aab3a53c35988374b9b7be1b67614e36f4

---
+++ _nuxt/static/1637151840/penielcho/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637151840",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpenielcho",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a28bb8b9"}}}}(null));
