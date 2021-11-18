c1ab7a7192f4185ddae60795670aa478af322df6
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-18 13:04:16 +0000

    deploy: 0b1b856ecc03e8ccdc10b8a858d2f80460480a05

---
+++ _nuxt/static/1637240650/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fall-articles","\u002Fapplication","\u002Fback-end","\u002Fcategories","\u002Fdata-science","\u002Fdevops","\u002Ffeatured","\u002Ffront-end","\u002Fgeneral","\u002Fprojects"]})
---
+++ _nuxt/static/1637240650/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637240650",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:void 0},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"10cbb6fa"}}}}(null,"\u002F"));
