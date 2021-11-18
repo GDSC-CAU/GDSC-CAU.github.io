47cfae74ca93ea5f04d87df4286fe4378a5acaae
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-18 08:35:37 +0000

    deploy: 9341caf134195d4f09ea3dd8e5a56ce5124cb33e

--- _nuxt/static/1637221274/wendyrhoads/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637221274",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwendyrhoads",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"33405192"}}}}(null));
