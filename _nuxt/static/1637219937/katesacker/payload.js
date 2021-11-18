50deef053505b7e4649ddcdf79f1218921ad7a47
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-18 07:41:20 +0000

    deploy: 1901fbde025a8dbd7d15a4ca3504c6b7d99a98c1

--- _nuxt/static/1637219937/katesacker/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/katesacker", (function(a,b){return {data:[{member:{slug:"katesacker",name:a,description:"뉴욕 지방검찰청에서 근무하고 있습니다.",role:"Core Member",img:"kate.JPG",dir:"\u002Fmembers",path:"\u002Fmembers\u002Fkatesacker",extension:".yaml",createdAt:b,updatedAt:b},memberArticles:[],authorName:a}],fetch:{},mutations:void 0}}("Kate Sacker","2021-11-18T07:18:04.924Z")));
--- _nuxt/static/1637219937/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637219937",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"82171751"}}}}(null));
