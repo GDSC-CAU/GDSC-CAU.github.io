e6336e0b7195075fa95c847abeacf6a23f1a9435
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-19 07:59:32 +0000

    deploy: 1b5842a4ee2b35df16be4d1d5680c8f79b582a85

---
+++ _nuxt/static/1637308766/heemin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637308766",layout:"default",error:a,serverRendered:true,routePath:"\u002Fheemin",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"12551d16"}}}}(null));
---
+++ _nuxt/static/1637308766/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637308766",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:void 0},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"12551d16"}}}}(null,"\u002F"));
