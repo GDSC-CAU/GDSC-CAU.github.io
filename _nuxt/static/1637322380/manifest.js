f71688ab99ef7f245227d6f4b58acd1c83cb0a69
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-20 11:12:59 +0000

    deploy: ac09205d169b75de84aa4f03e2658d846d00878c

--- _nuxt/static/1637322380/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fall-articles","\u002Fapplication","\u002Fback-end","\u002Fcategories","\u002Fdata-science","\u002Fdevops","\u002Ffeatured","\u002Ffront-end","\u002Fgeneral","\u002Fmembers","\u002Fprojects","\u002Fuhyeon","\u002Feunsol","\u002Fheemin","\u002Filgoo","\u002Fpenielcho","\u002Fsonggyeong","\u002Ftim-cook"]})
---
+++ _nuxt/static/1637406773/minjun/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/minjun", (function(a,b){return {data:[{member:{slug:"minjun",name:a,description:"CAU 소프트웨어학부 21학번",role:"Member",img:"minjun.jpg",dir:"\u002Fmembers",path:"\u002Fmembers\u002Fminjun",extension:".yaml",createdAt:b,updatedAt:b},memberArticles:[],authorName:a}],fetch:{},mutations:void 0}}("MinJun Choi","2021-11-20T11:11:58.271Z")));
