46ed0347fa40cb7483cc0220642fe1d20f8d05f3
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-22 12:03:04 +0000

    deploy: ec3f35f13c3538757a400e16caa8c8071e789531

--- _nuxt/static/1637563944/member/uhyeon/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/member/uhyeon", (function(a,b){return {data:[{member:{slug:"uhyeon",name:a,description:"프로그래밍을 위한 공부가 아니라, 세상과 부딫히는 개발을 원하는 사람입니다.",role:"Lead",img:"uhyeon.JPG",dir:"\u002Fmembers",path:"\u002Fmembers\u002Fuhyeon",extension:".yaml",createdAt:b,updatedAt:b},memberArticles:[],authorName:a}],fetch:{},mutations:void 0}}("UHyeon Jeong","2021-11-22T06:50:58.000Z")));
---
+++ _nuxt/static/1637582577/front-end/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637582577",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffront-end",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"070ec541"}}}}(null));
