739a74cccb1da7973b93b3df529781d32eae528d
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-23 05:37:12 +0000

    deploy: 96c328b0222bd17bb8ea9e6fbbe51dd7171aa9d1

--- _nuxt/static/1637645749/article/tailwind-on-nuxt/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637645749",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Ftailwind-on-nuxt",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"04a82faf"}}}}(null));
---
+++ _nuxt/static/1637645823/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637645823",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:void 0},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cd177cc4"}}}}(null,"\u002F"));
