e958e46975b12f6e2701da6f2ba035bb31efe943
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-23 18:18:27 +0000

    deploy: 171f60cd6f6bb75f7fc79089a74401b860e3e9f6

--- _nuxt/static/1637688940/back-end/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/back-end", (function(a,b){return {data:[{articles:[{slug:"spring-study-preview",description:"스프링 스터디와 1주차 스터디 이전의 내용에 대해 간단히 소개합니다.",title:"스프링 스터디-preview",category:a,author:b},{slug:"spring-study-week1",description:"Spring Data Jpa를 활용한 RDB 관리에 대해 소개합니다.",title:"스프링 스터디-1주차",category:a,author:b}]}],fetch:{},mutations:void 0}}("Back-End","IlGoo Yeo")));
