171f60cd6f6bb75f7fc79089a74401b860e3e9f6
Peniel Cho <penielcho98@gmail.com>
2021-11-24 03:16:55 +0900

    bto nuxt-content

--- nuxt.config.js
+++ nuxt.config.js
@@ -141,7 +141,7 @@ export default {
   modules: [
 
     //https://github.com/dword-design/nuxt-content-git
-    'nuxt-content-git',
+    // 'nuxt-content-git',
 
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
