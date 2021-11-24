003fbceb0534bbb29d394d1d6bbc796604688e31
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-24 14:57:26 +0000

    deploy: d1cbf1cf790fc99257053066028acb564a0e3e9e

--- _nuxt/static/1637746578/Data-Science/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/Data-Science", {data:[{articles:[{slug:"github-ipynb-loading",description:"ipynb 파일이 깃헙에서만 열리지 않는다면 렌더링 오류가 발생한 것이다. 해결 방법에 대해 알아보자.",title:"깃헙에서 ipynb 파일 로딩이 안돼요.",img:"ipython-github-error.png",category:"Data-Science",author:"Peniel Cho"}]}],fetch:{},mutations:void 0});
