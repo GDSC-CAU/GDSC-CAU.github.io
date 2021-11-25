5d98201ff9251d5af611d2880b642573135ce9dd
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-25 15:25:47 +0000

    deploy: 0199f62a2f79934835e41546c9b727e5e442147c

--- _nuxt/static/1637853077/member/minjun/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637853077",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmember\u002Fminjun",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5f96d059"}}}}(null));
