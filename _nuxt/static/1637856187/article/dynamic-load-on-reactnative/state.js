31e63d2515d5aed84a2be976f688919618b1c10c
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-25 16:03:16 +0000

    deploy: d6af72f5be05acf71d78eb8b08a89adb34092870

---
+++ _nuxt/static/1637856187/article/dynamic-load-on-reactnative/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637856187",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fdynamic-load-on-reactnative",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d8ea7a6e"}}}}(null));
