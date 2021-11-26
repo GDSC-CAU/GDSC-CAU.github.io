52b69f2ddbe146405d5f39814d87b9bcfd91d686
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-26 06:37:11 +0000

    deploy: 69679b1e0321e44d2eb68c2859ced62464a52ab0

---
+++ _nuxt/static/1637908622/article/how-to-set-up-my-profile/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637908622",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fhow-to-set-up-my-profile",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5b81d16d"}}}}(null));
