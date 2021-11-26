b321ce9514de3b2e9e14b92f7a507a3c601f59d1
thepenielcho <thepenielcho@users.noreply.github.com>
2021-11-26 07:34:28 +0000

    deploy: 973f66314e770b43cac1ff5f0ffa4f7b682164a3

--- _nuxt/static/1637909959/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637909959",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:void 0},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e56a7db9"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1637912057/front-end/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637912057",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffront-end",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e0259bba"}}}}(null));
