3eea20c8990554038c7651b20baf69f6bafd68d2
thepenielcho <thepenielcho@users.noreply.github.com>
2021-12-28 10:24:48 +0000

    deploy: 9b4fc19eca300c3747e8b23e82fa3316289f298c

---
+++ _nuxt/static/1640687074/Front-End/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640687074",layout:"default",error:a,serverRendered:true,routePath:"\u002FFront-End",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"db868dc2"}}}}(null));
