467ac1793c0cacdbd85dc9a7f64812203e0e3b24
thepenielcho <thepenielcho@users.noreply.github.com>
2022-01-07 06:45:50 +0000

    deploy: d536497673511054db5f47fd0c3380b373857079

--- _nuxt/static/1641536906/member/seoyeon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641536906",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmember\u002Fseoyeon",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"82b74bc5"}}}}(null));
