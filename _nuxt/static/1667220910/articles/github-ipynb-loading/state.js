c4b91fd5745578fe336dab3539d0562612df0da7
minusmo <minusmo@users.noreply.github.com>
2022-10-31 12:55:22 +0000

    deploy: c2bdb1b45e3da596cbc677f0e9d91f41ce92f2d2

---
+++ _nuxt/static/1667220910/articles/github-ipynb-loading/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667220910",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fgithub-ipynb-loading",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3d96b587"}}}}(null));
