2a7e111ccd2edc9ac4641df4c6f51aa6784e4b91
minusmo <minusmo@users.noreply.github.com>
2022-11-01 04:44:56 +0000

    deploy: 426379955df16dc28c6bee23a5747be862a8a9f6

---
+++ _nuxt/static/1667277878/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667277878",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:void 0},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"edc41ebb"}}}}(null,"\u002F"));
