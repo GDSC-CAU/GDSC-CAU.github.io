abfe4803d0ef21cfbbeb1f1a03d7b5a3cce831f5
minusmo <minusmo@users.noreply.github.com>
2022-11-03 17:11:58 +0000

    deploy: 31f6b9ec84d117d348671a93f3b972d59edbd4df

---
+++ _nuxt/static/1667495503/articles/django-study-week1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667495503",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fdjango-study-week1",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"91a0f377"}}}}(null));
