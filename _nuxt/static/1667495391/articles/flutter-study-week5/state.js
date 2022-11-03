8b18f74ebe2504c69921a962e7a7e9d47bb593a8
minusmo <minusmo@users.noreply.github.com>
2022-11-03 17:10:05 +0000

    deploy: e522fb9e6d508a00fffc448d90c29a97e29d3137

---
+++ _nuxt/static/1667495391/articles/flutter-study-week5/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667495391",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fflutter-study-week5",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7a39abf8"}}}}(null));
