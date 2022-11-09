c1ae264996d89313e1f6464878a2695ac59c1b62
minusmo <minusmo@users.noreply.github.com>
2022-11-09 08:22:45 +0000

    deploy: f8f42f28124a16c5ce67f370a342dfe4a74a0470

--- _nuxt/static/1667831815/members/penielcho/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667831815",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmembers\u002Fpenielcho",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cb13a1b9"}}}}(null));
---
+++ _nuxt/static/1667982146/members/yujin/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/members/yujin", (function(a,b){return {data:[{member:{slug:"yujin",name:a,description:"GDSC 2기 멤버로 활동하고 있습니다.",role:"Member",img:"yujin.jpg",dir:"\u002Fmembers",path:"\u002Fmembers\u002Fyujin",extension:".yaml",createdAt:b,updatedAt:b},memberArticles:[],authorName:a,proArticles:[]}],fetch:{},mutations:void 0}}("YuJin","2022-11-08T15:26:52.000Z")));
