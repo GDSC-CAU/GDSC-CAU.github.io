2c26ca1e68531e7da886ddea1cc1cdc93be188ec
minusmo <minusmo@users.noreply.github.com>
2022-11-11 02:41:29 +0000

    deploy: 0f176e6a8e473d64397fac459713cfdeda54415f

--- _nuxt/static/1668133179/members/jeongwon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668133179",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmembers\u002Fjeongwon",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"16d93183"}}}}(null));
