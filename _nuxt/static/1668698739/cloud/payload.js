0b2a135c65f6b95ee1c0fb584d8a2fb267f4e9aa
minusmo <minusmo@users.noreply.github.com>
2022-11-18 14:17:04 +0000

    deploy: ff9dc1cec63e60be07884d01283d2b472f4ae1ab

--- _nuxt/static/1668698739/cloud/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/cloud", {data:[{articles:[]}],fetch:{},mutations:void 0});
--- _nuxt/static/1668698739/members/jeongyoon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668698739",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmembers\u002Fjeongyoon",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2f080eaf"}}}}(null));
