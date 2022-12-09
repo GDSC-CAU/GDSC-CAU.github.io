82d61c80ac8874ca710ee620b6b54483e975d00d
yymin1022 <yymin1022@users.noreply.github.com>
2022-12-09 08:21:54 +0000

    deploy: d468aa2f6b79d842522a57ad85983282f73a8de2

---
+++ _nuxt/static/1670574091/data-science/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1670574091",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdata-science",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fced39f1"}}}}(null));
